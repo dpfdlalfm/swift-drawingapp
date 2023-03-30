d9da80bc8eb99c547b4d1837fa00eddb58b112d0
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2023-03-30 17:33:15 +0900

    [fix] 메소드에 private 접근 제어자 설정

--- DrawingApp/DrawingApp/RandomValue/RandomId.swift
+++ DrawingApp/DrawingApp/RandomValue/RandomId.swift
@@ -8,7 +8,6 @@ struct RandomId {
         let thirdIndex = uuid.index(uuid.startIndex, offsetBy: 3)
         uuid.insert("-", at: thirdIndex)
         let sixthIndex = uuid.index(thirdIndex, offsetBy: 4)
-        
         uuid.insert("-", at: sixthIndex)
         self.id = String(uuid)
     }
--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -15,13 +15,13 @@ class ViewController: UIViewController {
         }
     }
     
-    func getSafeAreaPoint() -> Point {
+    private func getSafeAreaPoint() -> Point {
         let safeAreaPointX = Double(self.view.layoutMargins.top)
         let safeAreaPointY = Double(self.view.layoutMargins.bottom)
         return Point(x: safeAreaPointX, y: safeAreaPointY)
     }
     
-    func getScreenSize() -> Size {
+    private func getScreenSize() -> Size {
         let screenWidth = Double(UIScreen.main.bounds.width)
         let screenHeight = Double(UIScreen.main.bounds.height)
         return Size(width: screenWidth, height: screenHeight)
