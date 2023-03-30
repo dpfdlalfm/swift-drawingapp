1eb508ebf8984dfaa65df1c6b24e1250f8216d5d
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2023-03-30 11:48:37 +0900

    [feat] safeArea 좌표와 화면크기 얻는 메소드 추가

--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -1,8 +1,29 @@
 import UIKit
+import OSLog
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let safeAreaPoint = getSafeAreaPoint()
+        let screenSize = getScreenSize()
+        
+        let factory = RectangleViewFactory(deviceSafeArea: safeAreaPoint, deviceScreen: screenSize)
+        
+    }
+    
+    func getSafeAreaPoint() -> Point {
+        let safeAreaPointX = Double(self.view.layoutMargins.top)
+        let safeAreaPointY = Double(self.view.layoutMargins.bottom)
+        return Point(x: safeAreaPointX, y: safeAreaPointY)
+    }
+    
+    func getScreenSize() -> Size {
+        let screenWidth = Double(UIScreen.main.bounds.width)
+        let screenHeight = Double(UIScreen.main.bounds.height)
+        return Size(width: screenWidth, height: screenHeight)
     }
 }
