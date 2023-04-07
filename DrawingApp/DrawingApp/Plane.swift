4b9241084f1cbe0d8955e5205ae9f16f7d3f8457
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2023-04-07 11:15:16 +0900

    [fix] Plane 구조체에서 사용하는 타입 변경
    - 프로토콜 타입을 사용하면 접근할 프로퍼티 증가로 보안성이 떨어져 구체타입인 Rectangle로 변경

--- DrawingApp/DrawingApp/Plane.swift
+++ DrawingApp/DrawingApp/Plane.swift
@@ -1,23 +1,29 @@
+import Foundation
+
 struct Plane {
-    private var figures:[Figure]
-    private var count: Int {
+    // subscript로 반환해주는 역할을 하기때문에 외부에서 직접적으로 접근하지 않고
+    // Plane 내에서 알아서 하도록 하면 되겠다 생각하여 private을 추가하였습니다.
+    private var rectangles:[Rectangle]
+    var count: Int {
         get {
-            return figures.count
+            return rectangles.count
         }
     }
     
     init() {
-        self.figures = []
+        self.rectangles = []
     }
     
-    subscript(index: Int) -> Figure? {
+    subscript(index: Int) -> Rectangle? {
         guard index < count else {
             return nil
         }
-        return figures[index]
+        return rectangles[index]
     }
     
-    mutating func add(figure: Figure) {
-        figures.append(figure)
+    mutating func add(rectangle: Rectangle) {
+        rectangles.append(rectangle)
     }
+    
+    
 }
