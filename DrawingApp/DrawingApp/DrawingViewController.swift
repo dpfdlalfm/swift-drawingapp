5f4298428780cdaec395cf53a7503b1a373bfcc5
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2023-04-11 11:44:41 +0900

    [add] 빈 공간 선택시 뷰 선택 취소 구현
    - 추가로 IBAction 메소드와 분리하기 위해 함수들 확장하여 작성

--- DrawingApp/DrawingApp/DrawingViewController.swift
+++ DrawingApp/DrawingApp/DrawingViewController.swift
@@ -1,12 +1,13 @@
 import UIKit
 import OSLog
 
-class DrawingViewController: UIViewController, UIGestureRecognizerDelegate {
-    let tapGestureRecognizer = UITapGestureRecognizer()
+class DrawingViewController: UIViewController {
+    var tapGestureRecognizer = UITapGestureRecognizer()
     var rectangleFactory: RectangleFactory?
     var plane = Plane()
+    var selectedView:[UIView] = []
     var figureViews: Dictionary<Id, UIView> = [:]
-    var logger: Logger?
+    var logger: Logger = Logger(subsystem: "com.inwoo.DrawingApp", category: "ViewController")
     
     @IBOutlet weak var figureInsperctorView: UIView!
     @IBOutlet weak var figureInspectorHideButton: UIButton!
@@ -14,16 +15,11 @@ class DrawingViewController: UIViewController, UIGestureRecognizerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tapGestureRecognizer.delegate = self
-        
+        tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tap(_:)))
+        self.view.addGestureRecognizer(tapGestureRecognizer)
         figureInspectorHideButton.layer.cornerRadius = CGFloat(15)
         rectangleButton.layer.cornerRadius = 40
         rectangleButton.layer.cornerCurve = .continuous
-        
-        guard logger == nil else {
-            return
-        }
-        logger = Logger(subsystem: "com.inwoo.DrawingApp", category: "ViewController")
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -37,7 +33,7 @@ class DrawingViewController: UIViewController, UIGestureRecognizerDelegate {
         }
     }
     
-    @IBAction func createRectangle(_ sender: Any) {
+    @IBAction func createRectangleView(_ sender: UIButton) {
         guard let rectangle = rectangleFactory?.create() else { return }
         
         let rectangleView = UIView(
@@ -46,29 +42,44 @@ class DrawingViewController: UIViewController, UIGestureRecognizerDelegate {
                          width: rectangle.size.width,
                          height: rectangle.size.height))
         rectangleView.backgroundColor = getUIColor(by: rectangle.color, with: rectangle.alpha)
-        rectangleView.addGestureRecognizer(tapGestureRecognizer)
+        self.view.addSubview(rectangleView)
         
-        guard let level = self.view.subviews.firstIndex(of: rectangleView) else {
-            return
-        }
-        
+        // Plane에서 mutating 키워드를 사용하여 추가하는 대신 클로저를 이용하여 구현했습니다.
+        // 근거는 클로저로 해당 기능을 구현했을 때, plane구조체의 주소값이 바뀌지 않는 장점이 있었기 때문입니다.
         let closure = { (rectangle: Rectangle, level: Int) in
             self.plane.rectangles += [(rectangle, level)]
+        }
+        guard let level = self.view.subviews.firstIndex(of: rectangleView) else {
+            return
         }
         closure(rectangle, level)
         
         self.figureViews[rectangle.id] = rectangleView
-        self.view.addSubview(rectangleView)
+        view.bringSubviewToFront(sender)
     }
     
     @IBAction func hideInspector(_ sender: Any) {
         
     }
     
-    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
-        return true
+    @objc func tap(_ sender: UITapGestureRecognizer) {
+        guard let rectangle = plane.isExist(in: sender.location(in: self.view)) else {
+            selectedView.forEach {
+                $0.layer.borderWidth = 0
+            }
+            selectedView.removeAll()
+            return
+        }
+        
+        if let rectangleView = figureViews[rectangle.id] {
+            rectangleView.layer.borderWidth = 5
+            rectangleView.layer.borderColor = UIColor.blue.cgColor
+            selectedView.append(rectangleView)
+        }
     }
-    
+}
+
+extension DrawingViewController {
     // Color를 UIColor로 변경하여 return 하는 메소드
     private func getUIColor(by color: Color, with alpha: Alpha) -> UIColor {
         let rgbMaxValue = 255.0
